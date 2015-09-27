4e7f1835e6a6e5f3a97e73d513d26b3bf9d20597
bdauria <bruno.dauria1@gmail.com>
2015-09-27 15:34:37 +0200

    edit make_links.sh

--- make_links.sh
+++ make_links.sh
@@ -25,7 +25,7 @@ echo "...done"
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
+    mv ~/$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/$file
 done
