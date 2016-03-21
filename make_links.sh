91af4a6dbe47d7ee11ad6a161170711e14974693
bdauria <bruno.dauria1@gmail.com>
2016-03-21 19:11:12 +0100

    Normalize file names (without . prefix), add vim folder and correct make_links script

--- make_links.sh
+++ make_links.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files=".tmux.conf .vimrc .vimrc.map .vimrc.vam .vimrc.bepo .vimrc.plugin"    # list of files/folders to symlink in homedir
+files="vim tmux.conf vimrc vimrc.map vimrc.vam vimrc.bepo vimrc.plugin"    # list of files/folders to symlink in homedir
 
 ##########
 
@@ -25,7 +25,7 @@ echo "...done"
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/$file ~/dotfiles_old/
+    mv ~/.$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/$file
+    ln -s $dir/$file ~/.$file
 done
